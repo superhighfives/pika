7830c275f54d5a76301fa7495b5569aa449f1107
Charlie Gleason <hi@charliegleason.com>
2021-02-15 01:10:43 +0000

    Improved swap button

--- Pika/Styles/SwapButtonStyle.swift
+++ Pika/Styles/SwapButtonStyle.swift
@@ -18,32 +18,40 @@ struct SwapButtonStyle: ButtonStyle {
                 ? Color(red: 0.1, green: 0.1, blue: 0.1)
                 : Color(red: 0.95, green: 0.95, blue: 0.95)
 
-            configuration.label
-                .padding(8)
-                .background(
-                    ZStack {
-                        Circle()
-                            .fill(bgColor)
-                            .shadow(
-                                color: Color.black.opacity(0.2),
-                                radius: configuration.isPressed ? 1 : 2,
-                                x: 0,
-                                y: configuration.isPressed ? 1 : 2
-                            )
-                            .overlay(
-                                Circle()
-                                    .stroke(fgColor.opacity(0.1))
-                            )
-                    }
-                )
-                .onHover { hover in
-                    self.isHovered = hover
+            HStack {
+                configuration.label
+                if isHovered {
+                    Text("Swap")
+                        .font(.system(size: 12.0))
                 }
-                .scaleEffect(self.isHovered ? 1.1 : 1.0)
-                .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
-                .opacity(isVisible ? 1.0 : 0.0)
-                .foregroundColor(fgColor.opacity(0.8))
-                .animation(.spring())
+            }
+            .padding(.horizontal, isHovered ? 12 : 8)
+            .padding(.vertical, 8)
+            .mask(RoundedRectangle(cornerRadius: 100.0, style: .continuous))
+            .background(
+                ZStack {
+                    RoundedRectangle(cornerRadius: 100.0, style: .continuous)
+                        .fill(bgColor)
+                        .shadow(
+                            color: Color.black.opacity(0.2),
+                            radius: configuration.isPressed ? 1 : 2,
+                            x: 0,
+                            y: configuration.isPressed ? 1 : 2
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 100.0, style: .continuous)
+                                .stroke(fgColor.opacity(0.1))
+                        )
+                }
+            )
+            .onHover { hover in
+                self.isHovered = hover
+            }
+            .scaleEffect(self.isHovered ? 1.1 : 1.0)
+            .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
+            .opacity(isVisible ? 1.0 : 0.0)
+            .foregroundColor(fgColor.opacity(0.8))
+            .animation(.spring())
         }
     }
 
